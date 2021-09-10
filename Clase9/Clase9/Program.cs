927d5a2077ab28db4c3da0d011d9ba3b46210af7
Alejo Zanni <56770851+alejozanni@users.noreply.github.com>
2021-09-09 22:56:53 -0300

    Add files via upload

---
+++ Clase9/Clase9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase9.Properties;
+
+namespace Clase9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion.Menu();
+
+        }
+    }
+}
---
+++ Clase9/Clase9/Properties/Operacion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Clase9.Properties
+{
+    public static class Operacion
+    {
+        private static string option;
+        private static double a;
+        public static double b;
+        
+
+        public static void Menu()
+        {
+            Console.WriteLine("Seleccione la operacion que desea realizar: ");
+            Console.WriteLine("S. Suma");
+            Console.WriteLine("R. Resta");
+            option = Console.ReadLine();
+        
+            switch (option)
+            {
+                case "S":
+                    Console.WriteLine("Elegiste la opcion Suma.");
+                    Console.WriteLine("Ingrese el primer numero: ");
+                    a=Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero: ");
+                    b=Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Suma igual a : " + suma(a,b));
+                    break;
+                
+               case "R":
+                   Console.WriteLine("Elegiste la opcion Resta.");
+                   Console.WriteLine("Ingrese el primer numero: ");
+                   a=Double.Parse(Console.ReadLine());
+                   Console.WriteLine("Ingrese el segundo numero: ");
+                   b=Double.Parse(Console.ReadLine());
+                   Console.WriteLine("Resta igual a : " + resta(a,b));
+                   break;
+            }
+        }
+        
+
+
+
+        public static double suma(double first, double second)
+        {
+            return first + second;
+        }
+
+
+        public static double resta(double first, double second)
+        {
+            return first - second;
+        }
+
+      
+    }
+}
